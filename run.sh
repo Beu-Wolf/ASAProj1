def82a78d51c46a9a3ef91c459550cfa71d36b66
Beu-Wolf <daniel.gseara@gmail.com>
2019-03-18 13:28:57 +0000

    valgrind + descricao solucao report

--- run.sh
+++ run.sh
@@ -7,8 +7,14 @@ echo Running tests...
 for i in {1..9}
 do
 	echo "======= Test #${i} ======="
+	echo "Time: "
 	time -p ./a.out < inputs/$i.in > tmp
 	diff tmp inputs/$i.out | grep real
+	echo "Valgrind call: "
+	valgrind ./a.out < inputs/$i.in | grep HEAP
+	
+	
+
 done
 
 echo Cleaning up...
